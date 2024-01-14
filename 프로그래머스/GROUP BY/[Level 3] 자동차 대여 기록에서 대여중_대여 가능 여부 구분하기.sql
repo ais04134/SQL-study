58f6a59983291bc235dfac63fd9e8d4a3ffa2ed5
Geon <is04133333@gmail.com>
2024-01-14 13:16:44 +0900

    [Level 3] 자동차 대여 기록에서 대여중_대여 가능 여부 구분하기

--- 프로그래머스/GROUP BY/[Level 3] 자동차 대여 기록에서 대여중_대여 가능 여부 구분하기.sql
+++ 프로그래머스/GROUP BY/[Level 3] 자동차 대여 기록에서 대여중_대여 가능 여부 구분하기.sql
@@ -1,8 +1,11 @@
-SELECT CAR_ID,
-MAX(CASE WHEN '2022-10-16' BETWEEN DATE_FORMAT(START_DATE, '%Y-%m-%d') AND DATE_FORMAT(END_DATE, '%Y-%m-%d')
+SELECT car_id,
+(CASE WHEN car_id IN (
+    SELECT car_id
+    FROM car_rental_company_rental_history
+    WHERE '2022-10-16' BETWEEN DATE_FORMAT(start_date, '%Y-%m-%d') AND DATE_FORMAT(end_date, '%Y-%m-%d'))
 THEN '대여중'
 ELSE '대여 가능'
 END) AS 'AVAILABILITY'
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-GROUP BY CAR_ID
-ORDER BY CAR_ID DESC
+FROM car_rental_company_rental_history
+GROUP BY car_id
+ORDER BY car_id DESC;
